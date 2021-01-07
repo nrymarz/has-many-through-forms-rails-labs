2463ae6c5b99e184a0617ce56de1458e9c594edd
nrymarz <nrymarz@gmail.com>
2021-01-06 22:48:21 -0600

    commit tree c7193bda9109600e6426c19de3337926c3b35b2a

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,12 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user
 
+  def user_attributes=(user_attributes)
+    if !user_attributes[:username].blank?
+      user = User.find_or_create_by(username: user_attributes[:username])
+      self.user = user
+    end
+  end
 end
